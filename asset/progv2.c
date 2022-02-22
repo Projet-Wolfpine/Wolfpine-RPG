50f65a534208ab0cdcf5a0e1bca294f3d7290b75
Ilyas <98398304+ilyaizu@users.noreply.github.com>
2022-02-22 12:01:59 +0100

    Update progv2.c

--- asset/progv2.c
+++ asset/progv2.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 //Lors du parcours de matrice puisque que les premiers crochet correspondes à la vertical et pas l'horizontal il faudra commencer par mettre les coordonnés Y puis X pour les fonctions
@@ -49,29 +50,43 @@ void afficher_mat(objet_t mat[Y][X]){
 }
 
 //Renvoi vrai si la case est libre
-int case_libre (objet_t mat[Y][X], int coord_x, int coord_y){
+int case_libre (objet_t mat[Y][X], int coord_y, int coord_x){
   return(mat[coord_y][coord_x].col == 0);
 }
 
-void placer_objet(objet_t mat[Y][X], int coord_x, int coord_y, char* id, int col){
+//Fonction qui permet de placer un objet dans la matrice
+void placer_objet(objet_t mat[Y][X], int coord_y, int coord_x, char* id, int col){
   if(case_libre(mat,coord_y,coord_x)){
     mat[coord_y][coord_x].col = col; //ici le chiffre 2 correspond à un objet traversable, le chiffre 1 correspond à une collision et 0 correspond à une valeur sans aucun objet
     mat[coord_y][coord_x].id = id;
   }
 }
 
-void info_objet(objet_t mat[Y][X], int coord_x, int coord_y){
-  //if(!case_libre(mat,coord_y,coord_x)){ //ne marche pas mais est pourtant necessaire pour éviter une erreur de segmentation
+//Nous permet de savoir ce que contient une case
+void info_objet(objet_t mat[Y][X], int coord_y, int coord_x){
+  if(case_libre(mat,coord_y,coord_x)){//ne marche pas mais est pourtant necessaire pour éviter une erreur de segmentation
+    printf("L'emplacement en Y = %d et X = %d ne contient aucun objet\n", coord_y, coord_x);
+  }else{
     printf("L'objet stocké en Y = %d et X = %d est : %s\n",coord_y, coord_x, mat[coord_y][coord_x].id);
     printf("Son statut de collision est de : %d\n",mat[coord_y][coord_x].col);
-  //}
+  }
 }
+
+//Creation du systeme de combat
+
+
 
 int main(){
   objet_t mat[Y][X];
   init_mat(mat);
   contour_mat(mat);
   placer_objet(mat,1,3,"palmier",1);
+  placer_objet(mat,2,5,"route",2);
+  placer_objet(mat,3,3,"personnage",3);
   afficher_mat(mat);
-  info_objet(mat,2,4);
+  info_objet(mat,1,3);
+  info_objet(mat,2,2);
+
+  info_objet(mat,2,5);
+  info_objet(mat,3,3);
 }
