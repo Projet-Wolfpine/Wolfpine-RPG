924797a96707b5e5b278e9f1e4aa443fca6fdf86
Ilyas <98398304+ilyaizu@users.noreply.github.com>
2022-02-22 12:03:07 +0100

    Update prog.c

--- asset/prog.c
+++ asset/prog.c
@@ -7,6 +7,10 @@
   char * id;
   int col;
 }*/
+typedef struct perso_s{
+  int anc_coord_x;
+  int anc_coord_y;
+}perso_t;
 
 //Fonction de création et d'initialisationn d'une matrice vide (remplie de 0)
 void init_mat(int mat[Y][X]){
@@ -51,8 +55,9 @@ int case_libre (int mat[Y][X], int coord_x, int coord_y){
 
 //Place le personnage au debut de partie
 void placer_pers(int mat[Y][X], int coord_x, int coord_y){
-  if(case_libre(mat,coord_y,coord_x))
+  if(case_libre(mat,coord_y,coord_x)){
     mat[coord_y][coord_x]=2;
+  }
 }
 
 //Deplace le personnage (ne pas oublier de supprimer l'ancien personnage avant de placer le nouveau)
@@ -71,27 +76,68 @@ void jeu(int mat[Y][X], int anc_coord_x, int anc_coord_y){
   switch(direction){
     case 'z': deplacer_pers(mat,anc_coord_x,anc_coord_y-1,anc_coord_x,anc_coord_y);break;
     case 'q': deplacer_pers(mat,anc_coord_x-1,anc_coord_y,anc_coord_x,anc_coord_y);break;
-    case 's': deplacer_pers(mat,anc_coord_x,anc_coord_y+1,anc_coord_x,anc_coord_y);break;
-    case 'd': deplacer_pers(mat,anc_coord_x+1,anc_coord_y,anc_coord_x,anc_coord_y);break;
+    case 'd': deplacer_pers(mat,anc_coord_x,anc_coord_y+1,anc_coord_x,anc_coord_y);break;
+    case 's': deplacer_pers(mat,anc_coord_x+1,anc_coord_y,anc_coord_x,anc_coord_y);break;
     case 'Z': deplacer_pers(mat,anc_coord_x,anc_coord_y-1,anc_coord_x,anc_coord_y);break;
     case 'Q': deplacer_pers(mat,anc_coord_x-1,anc_coord_y,anc_coord_x,anc_coord_y);break;
-    case 'S': deplacer_pers(mat,anc_coord_x,anc_coord_y+1,anc_coord_x,anc_coord_y);break;
-    case 'D': deplacer_pers(mat,anc_coord_x+1,anc_coord_y,anc_coord_x,anc_coord_y);break;
-    default: printf("Saisir Z,Q,S ou D pour vous deplacer\n");scanf("%c",&direction); //changer le if ligne 68 (au dessus) pour le mettre à la place du default ici, ça evite la boucle en dessous aussi
+    case 'D': deplacer_pers(mat,anc_coord_x,anc_coord_y+1,anc_coord_x,anc_coord_y);break;
+    case 'S': deplacer_pers(mat,anc_coord_x+1,anc_coord_y,anc_coord_x,anc_coord_y);break;
+    default: printf("Saisir Z,Q,S ou D pour  deplacer\n");scanf("%c",&direction); //changer le if ligne 68 (au dessus) pour le mettre à la place du default ici, ça evite la boucle en dessous aussi
   }
 }
 
+
+//Modification des fonctions avec une structure personnage et des pointeurs sur entiers
+
+void placer_pers2(int mat[Y][X], int coord_x, int coord_y, perso_t * player){
+  if(case_libre(mat,coord_y,coord_x)){
+    mat[coord_y][coord_x]=2;
+    player->anc_coord_x = coord_x;
+    player->anc_coord_y = coord_y;
+  }
+}
+
+void deplacer_pers2(int mat[Y][X], int coord_x, int coord_y, perso_t * player){
+  if(case_libre(mat,coord_y,coord_x)){
+    placer_pers2(mat,coord_y,coord_x,player);
+    mat[player->anc_coord_y][player->anc_coord_x] = 0;
+  }
+}
+
+
+/*void jeu2(int mat[Y][X], perso_t player){
+  char direction;
+  printf("Saisir Z,Q,S ou D pour vous deplacer\n");
+  scanf("%c",&direction);
+  switch(direction){
+    case 'z': deplacer_pers2(mat,player.anc_coord_x,player.anc_coord_y-1,player); player.anc_coord_y -= 1;break;
+    case 'q': deplacer_pers2(mat,player.anc_coord_x-1,player.anc_coord_y,player); player.anc_coord_x -= 1;break;
+    case 'd': deplacer_pers2(mat,player.anc_coord_x,player.anc_coord_y+1,player); player.anc_coord_y += 1;break;
+    case 's': deplacer_pers2(mat,player.anc_coord_x+1,player.anc_coord_y,player); player.anc_coord_x += 1;break;
+    case 'Z': deplacer_pers2(mat,player.anc_coord_x,player.anc_coord_y-1,player); player.anc_coord_y -= 1;break;
+    case 'Q': deplacer_pers2(mat,player.anc_coord_x-1,player.anc_coord_y,player); player.anc_coord_x -= 1;break;
+    case 'D': deplacer_pers2(mat,player.anc_coord_x,player.anc_coord_y+1,player); player.anc_coord_y += 1;break;
+    case 'S': deplacer_pers2(mat,player.anc_coord_x+1,player.anc_coord_y,player); player.anc_coord_x += 1;break;
+    default: printf("Saisir Z,Q,S ou D pour  deplacer\n");scanf("%c",&direction);
+  }
+}
+*/
 //Programme principal
 int main (){
   int mat[Y][X];
+  perso_t player;
+
   init_mat(mat);
   contour_mat(mat);
-  placer_pers(mat, 1, 1);
-  while(1){//boucle infinie pour test (oui je sais que c moche)
+
+  placer_pers2(mat, 1, 1,&player);
+  /*while(1){//boucle infinie pour test (oui je sais que c moche)
     system("clear");
     afficher_mat(mat);
-    jeu(mat,1,1);
-  }
-  //afficher_mat(mat);
+    jeu2(mat,player);
+  }*/
+  afficher_mat(mat);
+  deplacer_pers2(mat,2,2,&player);
+  afficher_mat(mat);
   return 0;
 }
