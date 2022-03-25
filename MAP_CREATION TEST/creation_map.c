ca5b66f02b328d7634232313260a4fea9fdc7276
Dimitri  BOUGLE <97879281+im-MnX@users.noreply.github.com>
2022-03-25 09:49:41 +0100

    test avec une map

---
+++ MAP_CREATION TEST/creation_map.c
@@ -0,0 +1,131 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "deplacement.h"
+
+void lecture_fichier_map(char nom_fichier[20],int map_int[Y][X]){
+  /* lis un fichier.txt data de a map demandée,
+   et met chaque entier dans une case de la matrice*/
+  int i, j, var;
+  FILE * fic;
+  fic=fopen(nom_fichier,"r");
+  fscanf(fic,"%i", &var);
+  for(i=0;i<Y;i++){
+    for(j=0;j<X;j++){
+      map_int[i][j]=var;
+      fscanf(fic,"%i", &var);
+    }
+  }
+  fclose(fic);
+}
+
+
+void mise_struct_map(case_t map_info[Y][X], int map_int[Y][X]){
+  /* la fonction prend la matrice avec les entiers afin de  remplir un tableau
+  de structure contenant les infos de chaque cases (id_bloc et statut colision)*/
+  int i, j;
+  for(i=0;i<Y;i++){
+    for(j=0;j<X;j++){
+      switch(map_int[i][j]){
+        case 1:
+          map_info[i][j].id = "ID_MUR1";
+          map_info[i][j].col = 2;
+          break;
+        case 2:
+          map_info[i][j].id = "ID_MUR2";
+          map_info[i][j].col = 2;
+          break;
+        case 3:
+          map_info[i][j].id = "ID_SOL1";
+          map_info[i][j].col = 0;
+          break;
+        case 4:
+          map_info[i][j].id = "ID_LIT2";
+          map_info[i][j].col = 0;
+          break;
+        case 5:
+          map_info[i][j].id = "ID_LIT3";
+          map_info[i][j].col = 0;
+          break;
+        case 6:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 7:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 8:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 9:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 10:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 11:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 12:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 13:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 14:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 15:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 16:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 17:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 19:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+        case 20:
+          map_info[i][j].id = "ID_";
+          map_info[i][j].col = 0;
+          break;
+      }
+    }
+  }
+}
+
+
+int main(){
+  case_t map_info[Y][X];
+  int map_int[Y][X];
+  char nom_fichier[20];
+  printf("donnez le nom du fichier : ") ;
+  scanf("%s", nom_fichier) ;
+  lecture_fichier_map(nom_fichier, map_int);
+  for(int i = 0; i<Y;i++){
+    for(int j = 0; j<X;j++){
+      printf("%i ", map_int[i][j]);
+    }
+    printf("\n");
+  }
+  mise_struct_map(map_info,map_int);
+  printf("\n");
+  printf("\n");
+  printf("%d",map_info[0][0].col);
+  printf("\n");
+  printf("%s",map_info[0][0].id);
+  printf("\n");
+}
---
+++ MAP_CREATION TEST/deplacement.h
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "structures.h"
+
+//Lors du parcours de matrice puisque que les premiers crochet correspondes à la vertical et pas l'horizontal il faudra commencer par mettre les coordonnés Y puis X pour les fonctions
+#define Y 16
+#define X 30
+
+void init_mat(case_t mat[Y][X]);
+
+void contour_mat(case_t mat[Y][X]);
+
+void afficher_mat(case_t mat[Y][X]);
+
+int case_libre (case_t mat[Y][X], int coord_y, int coord_x);
+
+void placer_objet(case_t mat[Y][X], int coord_y, int coord_x, char* id, int col);
+
+void placer_pers(case_t mat[Y][X], int coord_y, int coord_x, perso_t  *player);
+
+void deplacer_pers(case_t mat[Y][X], int coord_y, int coord_x, perso_t  *player);
+
+void aller_gauche(case_t mat[Y][X], perso_t *player);
+
+void aller_droite(case_t mat[Y][X], perso_t *player);
+
+void aller_dessus(case_t mat[Y][X], perso_t *player);
+
+void aller_dessous(case_t mat[Y][X], perso_t *player);
+
+void info_objet(case_t mat[Y][X], int coord_y, int coord_x);//Pas forcement utile à voir pour supprimer
