0976d883570b9e698937738f752be750df0967ac
Tibo_Courcol <44385130+thibcour@users.noreply.github.com>
2022-03-26 15:37:37 +0100

    mise à jour

--- src/game.c
+++ src/game.c
@@ -5,77 +5,54 @@
 #include "menu_principal.h"
 #include "deplacement.h"
 #include "game.h"
+#include "creation_map.h"
 
-//#define TAILLE_CASE_PXL 64
+#define TAILLE_CASE_PXL 32
 
-case_t mat[Y][X];
-perso_t joueur;
+perso_t *joueur;
+
 //joueur = malloc(sizeof(perso_t));
-
-
-void map(){
-	//fond_blanc();
-	int longeur = 1920;
-	int hauteur = 1080;
-	int longeur_mat = longeur/TAILLE_CASE_PXL;
-	int hauteur_mat = hauteur/TAILLE_CASE_PXL;
-	for(int i = 0;i<longeur_mat; i++) // i coresppond a x de la matrice et j coresppond a y en ordonnée 
-			for(int j = 0; j<hauteur_mat; j++)
-                	drawImage(i*TAILLE_CASE_PXL , j*TAILLE_CASE_PXL , "herbe.png", TAILLE_CASE_PXL, TAILLE_CASE_PXL );
-
-    drawImage(TAILLE_CASE_PXL*10 , TAILLE_CASE_PXL*5 , "perso.png", TAILLE_CASE_PXL, TAILLE_CASE_PXL );
-	placer_pers(mat,5,10,&joueur);//probleme
-    afficher_mat(mat);
-
-}
-
-
 
 void start(){
     SDL_RenderClear(renderer);
     int touche=-1;
-    int cpt=0;
     int running = 1;
-  	init_mat(mat);
-	contour_mat(mat);
-    afficher_mat(mat);
-    init_player(&joueur);
-    placer_pers(mat,5,10,&joueur);
-    map();
+  	//init_mat(mat);
+	//contour_mat(mat);
+    //afficher_mat(mat);
+    //init_player(&joueur);
+    //placer_pers(mat,5,10,&joueur);
+    afficher_map("map3.txt",64);
     faire_rendu();
-    while(running == 1){
-        //cpt++;
+    while(running == 1){  
         touche=touche_detecter();
-        //printf("%d\n",touche);
         if(touche != 0){
-            map();
+            afficher_map("map3.txt",64);
             if(touche == 1){
                 drawImage(TAILLE_CASE_PXL*15 , TAILLE_CASE_PXL*7 , "perso.png", TAILLE_CASE_PXL, TAILLE_CASE_PXL ); 
+                faire_rendu();
 
             }
             if(touche == 2){
-                drawImage(TAILLE_CASE_PXL*20 , TAILLE_CASE_PXL*15 , "perso.png", TAILLE_CASE_PXL, TAILLE_CASE_PXL );
-
+                drawImage(TAILLE_CASE_PXL*15 , TAILLE_CASE_PXL*8 , "perso.png", TAILLE_CASE_PXL, TAILLE_CASE_PXL );
+                faire_rendu();
+            }
+            if(touche == 3){
+                drawImage(TAILLE_CASE_PXL*16, TAILLE_CASE_PXL*8 , "perso.png", TAILLE_CASE_PXL, TAILLE_CASE_PXL );
+                faire_rendu();
+            }
+            if(touche == 4){
+                drawImage(TAILLE_CASE_PXL*14 , TAILLE_CASE_PXL*8 , "perso.png", TAILLE_CASE_PXL, TAILLE_CASE_PXL );
+                faire_rendu();
             }
             if(touche == 5){
                 running=0;
             }
             
-            faire_rendu();
             
 
-        }
-        /*if(cpt == 15){
-            running=0;
-        }*/
-
-
-        
+        }        
 
     }
-    //SDL_RenderClear(renderer);
     Menu();
-
-
-
 }
