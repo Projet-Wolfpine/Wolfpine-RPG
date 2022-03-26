6f335f7985778c80dbbf50aa1accd5117ab88eb9
Tibo_Courcol <44385130+thibcour@users.noreply.github.com>
2022-03-26 15:39:08 +0100

    mise à jour

---
+++ src/creation_map.h
@@ -0,0 +1,5 @@
+#include "deplacement.h"
+
+void lecture_fichier_map(char nom_fichier[20],int map_int[Y][X]);
+void mise_struct_map(case_t map_info[Y][X], int map_int[Y][X]);
+void afficher_map(char * nom_map,int taille);
