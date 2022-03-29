f4bd2524803e8c31a63c2cda2883cb262a4dd622
Ilyas <98398304+ilyaizu@users.noreply.github.com>
2022-03-29 22:06:19 +0200

    Update deplacement.h

--- asset/deplacement.h
+++ asset/deplacement.h
@@ -4,7 +4,7 @@
 
 
 //Lors du parcours de matrice puisque que les premiers crochet correspondes à la vertical et pas l'horizontal il faudra commencer par mettre les coordonnés Y puis X pour les fonctions
-#define Y 11
+#define Y 16
 #define X 31
 
 void init_mat(case_t mat[Y][X]);
@@ -15,21 +15,16 @@ void afficher_mat(case_t mat[Y][X]);
 
 int case_libre (case_t mat[Y][X], int coord_y, int coord_x);
 
-void placer_objet(case_t mat[Y][X], int coord_y, int coord_x, char* id, int col);
-
 void placer_pers(case_t mat[Y][X], int coord_y, int coord_x, perso_t  *player);
 
-void deplacer_pers(case_t mat[Y][X], int coord_y, int coord_x, perso_t  *player);
+int deplacer_pers(case_t mat[Y][X], int coord_y, int coord_x, perso_t  *player);
 
-void aller_gauche(case_t mat[Y][X], perso_t *player);
+int gauche(case_t mat[Y][X], perso_t *player);
 
-void aller_droite(case_t mat[Y][X], perso_t *player);
+int droite(case_t mat[Y][X], perso_t *player);
 
-void aller_dessus(case_t mat[Y][X], perso_t *player);
+int dessus(case_t mat[Y][X], perso_t *player);
 
-void aller_dessous(case_t mat[Y][X], perso_t *player);
-
-void info_objet(case_t mat[Y][X], int coord_y, int coord_x);//Pas forcement utile à voir pour supprimer
-
+int dessous(case_t mat[Y][X], perso_t *player);
 
 #endif
