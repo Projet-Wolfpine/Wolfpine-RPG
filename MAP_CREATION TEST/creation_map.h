4e5d70e6fd58da7b0eeeadf283611a9761704b10
Rattier Killian <92730061+Elziix@users.noreply.github.com>
2022-03-31 15:35:02 +0200

    Delete MAP_CREATION TEST directory

--- MAP_CREATION TEST/creation_map.h
+++
@@ -1,8 +0,0 @@
-#include "stdlib.h"
-#include "stdio.h"
-#include "deplacement.h"
-
-
-void lecture_fichier_map(char nom_fichier[20],int map_int[Y][X]);
-
-void mise_struct_map(case_t map_info[Y][X], int map_int[Y][X]);
