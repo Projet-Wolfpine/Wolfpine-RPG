3e32b43edc9a8e24880988288a63fa1a9514df99
Rattier Killian <92730061+Elziix@users.noreply.github.com>
2022-03-31 13:51:31 +0200

    Update tour_joueur SDL

--- src/combat.c
+++ src/combat.c
@@ -93,24 +93,124 @@ void init_monster(monstre_t * monster, char * name, int hp, int dgt, int armor){
 
 void tour_joueur(perso_t * player, monstre_t * monstre){
 	int choix = 0; 
-  int spell_num = 0;
-  int dgt;
+	int spell_num = 0;
+	int dgt;
+  
+  	if(player->hp > 0){
+		SDL_Event event;
+		int combat = -1;
+		int souris_x=0, souris_y=0;
+		int spell_choose = 0;
+		bool exit = FALSE;
 
-  if(player->hp > 0){
-    while(choix != 1 && choix != 2){
-      printf("Choisissez le mode d'attaque (1 : attaque , 2 : sort) : ");
-      scanf("%d",&choix);
-    }
-    
-    if(choix == 1)
-    {
-      dgt =  player->dgt - monstre->armor;
-      monstre->hp -= dgt;
-       if(monstre->hp < 0){
-          monstre->hp = 0;
-        }
-      printf("Vous infligez %d dégats au monstre	     ||	HP player : %d  HP monstre : %d\n", dgt, player->hp, monstre->hp);
-    }
+		SDL_GetMouseState(&souris_x, &souris_y);
+		while(combat == -1 && monstre->hp > 0){
+			drawImage(0, 0, "combat.png", 1920, 1080);
+			drawImage(700,200,"wolfy.png", 520, 520);
+			__itoa(player->hp,hp_joueur,10,3);
+			drawText(480,920,hp_joueur,30,30,noir);
+			drawText(850,150,monstre->name,40,40,noir);
+			__itoa(monstre->hp,hp_monstre,10,3);
+			drawText(1700,80,hp_monstre,30,30,noir);
+			faire_rendu();
+			
+		 	if (SDL_WaitEvent(&event) != 0) {
+				switch(event.type){
+				case SDL_MOUSEBUTTONDOWN:
+					SDL_GetMouseState(&souris_x, &souris_y);
+
+					if((souris_x >= 700 && souris_x <=825) && (souris_y >= 900 && souris_y <=950)){
+						dgt =  player->dgt - monstre->armor;
+						monstre->hp -= dgt;
+						if(monstre->hp < 0){
+							monstre->hp = 0;
+						}
+						/*__itoa(dgt,inf_dgt,10,2);
+						drawText(800,500,inf_dgt,25,25,noir);
+						faire_rendu();*/
+						combat = 0; break;
+					}
+
+					if((souris_x >= 1000 && souris_x <=1300) && (souris_y >= 900 && souris_y <=950)){
+						//while(spell_choose != TRUE){
+							drawImage(0, 0, "spells.png", 1920, 1080);
+							drawImage(700,200,"wolfy.png", 520, 520);
+							__itoa(player->hp,hp_joueur,10,3);
+							drawText(480,920,hp_joueur,30,30,noir);
+							drawText(850,150,monstre->name,40,40,noir);
+							__itoa(monstre->hp,hp_monstre,10,3);
+							drawText(1700,80,hp_monstre,30,30,noir);
+								
+							//Spells
+							drawText(650,920,player->spell[0]->name,30,30,noir);
+							drawText(900,920,player->spell[1]->name,30,30,noir);
+							drawText(1100,920,player->spell[2]->name,30,30,noir);
+							drawText(1300,920,player->spell[3]->name,30,30,noir);	
+							faire_rendu();
+							
+						
+
+						while(spell_choose != 1){
+							if (SDL_WaitEvent(&event) != 0) {
+								switch(event.type){
+								case SDL_MOUSEBUTTONDOWN:
+									SDL_GetMouseState(&souris_x, &souris_y);
+
+									if((souris_x >= 650 && souris_x <=800) && (souris_y >= 900 && souris_y <=950)){
+										if(spell_existe(player,0) != 0){
+											dgt = player->spell[0]->dgt - monstre->armor;
+											monstre->hp -= dgt;
+											if(monstre->hp < 0){
+												monstre->hp = 0;
+											}
+											spell_choose = 1;
+										}
+										else{
+											tour_joueur(player, monstre);
+											//combat = 0; break;
+										}
+									}
+
+
+									if((souris_x >= 900 && souris_x <=1050) && (souris_y >= 900 && souris_y <=950)){
+										if(spell_existe(player,1) != 0){
+											dgt = player->spell[0]->dgt - monstre->armor;
+											monstre->hp -= dgt;
+											if(monstre->hp < 0){
+												monstre->hp = 0;
+											}
+											spell_choose = 1;
+										}
+										else{
+											tour_joueur(player, monstre);
+											//combat = 0; break;
+										}
+									}
+								}
+							}
+							
+
+							
+							//SDL_Delay(5000);
+						}
+
+						combat = 0; break;
+						
+						
+						/*drawImage(700,200,"wolfy.png", 520, 520);
+						__itoa(player->hp,hp_joueur,10,3);
+						drawText(480,920,hp_joueur,30,30,noir);
+						drawText(850,150,monstre->name,40,40,noir);
+						__itoa(monstre->hp,hp_monstre,10,3);
+						drawText(1700,80,hp_monstre,30,30,noir);
+						faire_rendu();*/
+			
+					}
+				}
+			}
+		}
+
+	/*
     else if(choix == 2)
     {
       printf("Choisissez quel sort vous souhaitez utiliser.\n");
@@ -138,9 +238,10 @@ void tour_joueur(perso_t * player, monstre_t * monstre){
         }
       }
       printf("Vous infligez %d dégats magiques à %s  ||	HP player : %d  HP monstre : %d\n", dgt, monstre->name, player->hp, monstre->hp);
-    }
+    }*/
   }
 }
+
 
 void tour_monstre(perso_t * player, monstre_t * monstre)
 {
