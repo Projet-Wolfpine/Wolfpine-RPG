8eb5ff55a2e2dc519762de9c02e42cf5bd046068
Dimitri  BOUGLE <97879281+im-MnX@users.noreply.github.com>
2022-04-01 10:16:55 +0200

    Update creation_map.c

--- src/creation_map.c
+++ src/creation_map.c
@@ -431,6 +431,10 @@ void mise_struct_map(case_t map_info[Y][X], int map_int[Y][X]){
                 map_info[i][j].id = "PNJ01";
                 map_info[i][j].col = 3;
             }
+            if(map_int[i][j] == 901){
+                map_info[i][j].id = "PNJ_EPEE";
+                map_info[i][j].col = 3;
+            }
         }
     }
     clock_gettime(CLOCK_REALTIME, &end2);
